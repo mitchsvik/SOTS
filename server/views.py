d6a39484ddbe183a766c953cc4447c7f052ec302
Veniamin Zinevych <mitchsvik@gmail.com>
2018-04-12 22:53:04 +0300

    Task API connected to task manager.

--- server/views.py
+++ server/views.py
@@ -4,7 +4,7 @@ import uuid
 from flask import request
 from flask_restful import Resource
 
-from server.app import user_collection
+from server.app import user_collection, task_queue
 
 
 def remove_object_id(instance):
@@ -30,8 +30,8 @@ class User(Resource):
         if user is None:
             return {'error': 'User with id={} does not exists'.format(user_id)}, 404
         user_in_area = user_collection.find({
-            'x_pos': {'$lt': user['x_pos'] + 32, '$gt': user['x_pos'] - 32},
-            'y_pos': {'$lt': user['y_pos'] + 32, '$gt': user['y_pos'] - 32},
+            'x_pos': {'$lt': user['x_pos'] + 16, '$gt': user['x_pos'] - 16},
+            'y_pos': {'$lt': user['y_pos'] + 16, '$gt': user['y_pos'] - 16},
         })
         response = [remove_object_id(user_data) for user_data in user_in_area]
         return {'response': response}
@@ -58,6 +58,8 @@ class TaskList(Resource):
         )
 
         if update_data.modified_count == 1:
+            # Task signature ['time', 'user_id', 'task_id', 'is_active']
+            task_queue.put((task_end, user_id, task_id, True))
             return {'response': task_id}, 201
         else:
             return {'error': "Reached the limit of task list size"}, 429
@@ -68,11 +70,17 @@ class Task(Resource):
     Delete user task
     """
     def delete(self, user_id, task_id):
+        user_data = user_collection.find_one({'user_id': user_id})
+        matched_task = list(filter(lambda t: t.get('task_id') == task_id, user_data.get('task_list', [])))
+
         updated_data = user_collection.update_one(
             {'user_id': user_id},
             {'$pull': {'task_list': {'task_id': task_id}}}
         )
         if updated_data.modified_count == 1:
+            task_end = matched_task[0].get('task_end')
+            # Task signature ['time', 'user_id', 'task_id', 'is_active']
+            task_queue.put((task_end, user_id, task_id, False))
             return '', 204
         else:
             return {'error': 'Task not found or already expired'}, 404
